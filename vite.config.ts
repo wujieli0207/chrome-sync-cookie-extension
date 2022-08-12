94b5a924a1dccdee56f213a06c7e3342bd33f4f9
wujieli0207 <wujieli0207@outlook.com>
2022-08-12 22:49:19 +0800

    build: 引入 antdv 组件库

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import {
+  createStyleImportPlugin,
+  AndDesignVueResolve,
+} from "vite-plugin-style-import";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 按需引入组件
+    createStyleImportPlugin({
+      resolves: [AndDesignVueResolve()],
+    }),
+    // 自动导入组件
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
   server: {
     host: true,
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
   },
 });
